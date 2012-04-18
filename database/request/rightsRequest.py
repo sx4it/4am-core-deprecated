34d9e1ede0764159d4252ef5f7794a24091524f4
Lassi <lassi@sx4it.com>
2012-04-18 02:55:35 +0100

    - Fixed: rightsRequest.py

--- database/request/rightsRequest.py
+++ database/request/rightsRequest.py
@@ -14,23 +14,27 @@ class RightsRequest():
     def __init__(self, sess):
         self._session = sess
 
+    def getRights(self):
+        ret = self._session.query(rights.Rights).all()
+        return ret
+
     # Get the rights object associated to the given id
-    def getRightsById(sid):
-        ret = self.session.query(rights.Rights).filter_by(id=sid).one()
+    def getRightsById(self, sid):
+        ret = self._session.query(rights.Rights).filter_by(id=sid).one()
         return ret
 
     # Add the given mapped object to the database
     def addRights(seld, rights):
-        self.session.add(rights)
-        self.session.commit()
+        self._session.add(rights)
+        self._session.commit()
 
     # Delete the rights column associated to the given id
-    def removeRightsById(sid):
-        ret = self.session.query(rightsRights).filter_by(id=sid).one()
-        self.session.delete(ret)
-        self.session.commit()
+    def removeRightsById(self, sid):
+        ret = self._session.query(rightsRights).filter_by(id=sid).one()
+        self._session.delete(ret)
+        self._session.commit()
 
     # Delete the given mapped object from the database
-    def removeRights(rights):
-        self.session.delete(rights)
-        self.session.commit()
+    def removeRights(self, rights):
+        self._session.delete(rights)
+        self._session.commit()
